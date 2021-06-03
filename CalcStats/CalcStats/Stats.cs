0d44acc8167f531246c9fd7e2ba9c9a4211f0d9c
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2021-06-04 01:59:01 +0300

    Init Stats class

---
+++ CalcStats/CalcStats/Stats.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcStats
+{
+    public class Stats
+    {
+        public int Minimum(int[] array)
+        {
+            return 0;
+        }
+
+        public int Maximum(int[] array)
+        {
+            return 0;
+        }
+
+        public int Length(int[] array)
+        {
+            return 0;
+        }
+
+        public double Average(int[] array)
+        {
+            return 0;
+        }
+    }
+}
